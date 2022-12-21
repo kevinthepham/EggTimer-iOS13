a57930ca57188d15d6feec5ef5eb815fbc7909f3
Kevin Pham <phamthk@gmail.com>
2022-12-20 21:53:42 -0600

    Stable version where egg election prints desired time

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let softTime = 5
+    let medTime = 7
+    let hardTime = 12
+    
+    @IBAction func eggSelection(_ sender: UIButton) {
+        
+        var hardness = 0
+        
+        switch sender.currentTitle {
+        case "Soft":
+            hardness = softTime
+        case "Medium":
+            hardness = medTime
+        case "Hard":
+            hardness = hardTime
+        default:
+            print("Invalid egg hardness")
+        }
+        
+        print(hardness)
+    }
+    
 }
